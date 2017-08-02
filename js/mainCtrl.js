765a4596b26275c4a1c899ebe8f6a3e1e34d818c
Jess Poemape <jesspoemape@gmail.com>
2017-08-02 09:53:52 -0600

    user can add quotes to list of quotes. user can search through quotes

---
+++ js/mainCtrl.js
@@ -0,0 +1,10 @@
+angular.module('quoteBook').controller('mainCtrl', function($scope, dataService) {
+    $scope.quotes = dataService.getQuotes();
+    $scope.addQuote = function() {
+        let newQuote = {
+            text: $scope.text,
+            author: $scope.author
+        };
+        dataService.addData(newQuote);
+    }
+})
